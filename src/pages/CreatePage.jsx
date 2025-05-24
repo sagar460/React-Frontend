a39c4a73421365fec406826527767c16b1dd223e
sagar460 <shresthasagr7000@gmail.com>
2025-05-24 06:33:34 +0545

    new commit

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -132,6 +132,7 @@ function CreatePage() {
           <button
             className="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             type="submit"
+            onClick={submitData}
           >
             Save All
           </button>
