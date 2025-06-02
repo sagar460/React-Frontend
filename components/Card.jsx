21649db3e8054fe62b1b7edd47226032596dab58
sagar460 <shresthasagr7000@gmail.com>
2025-06-03 01:15:23 +0545

    last commit

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,7 +4,7 @@ function Card({ book }) {
   return (
     <Link to={`/single-page/${book.id}`}>
       <div className="flex px-3 py-3">
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-gray-300">
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{book.bookName}</div>
             <p className="text-gray-700 text-base">{book.bookAuthor}</p>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <header className="bg-white">
+    <header className="bg-orange-200">
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex-1 md:flex md:items-center md:gap-12">
